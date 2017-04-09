c21e9d964e3f2973584e61c9618245a8155c7d40
JLee <jlee9@albany.edu>
2017-04-08 23:49:39 -0400

    JJ

--- src/main/java/edu/albany/cs/base/PixelToAPDMData.java
+++ src/main/java/edu/albany/cs/base/PixelToAPDMData.java
@@ -2,8 +2,10 @@ package edu.albany.cs.base;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static java.lang.System.exit;
@@ -12,7 +14,7 @@ import static java.lang.System.exit;
 public class PixelToAPDMData {
     public static final String NAME = "";
     public static final int downsize = 2;
-    public static String POSTURE = "";
+    public static String Z0NAME = "";
     private int PIC_HEIGHT;
     private int PIC_WIDTH;
     private int PIXEL_COUNT;
@@ -20,44 +22,57 @@ public class PixelToAPDMData {
     private double[][] greyValues;
     private BufferedImage img;
 
-    public PixelToAPDMData(){
+    public PixelToAPDMData() {
         //temporary for now
-        PICTURE_COUNT = 12;
+        PICTURE_COUNT = 0;
         img = null;
     }
 
     public void generateSingleCase(String inputFiles, String outputFile) throws IOException {
-        this.greyValues     = getGreyLevels(inputFiles);
+        this.greyValues = getGreyLevels(inputFiles);
 
-        outputFile = outputFile + PIC_HEIGHT +"X"+ PIC_WIDTH + ".txt";
+        outputFile = outputFile + PIC_HEIGHT + "X" + PIC_WIDTH + ".txt";
         generatePixelAPDM(outputFile);
 
         //test if all connected
         testTrueSubGraph(outputFile);
     }
 
-    private double[][] getGreyLevels(String inputFiles) throws IOException{
+    private double[][] getGreyLevels(String inputDir) throws IOException {
         //an2i_left_neutral_open_2.png;
         int[][] greyValuesT = new int[PICTURE_COUNT][960];
 
-        greyValuesT[0] = getGreyLevelsFromImages(inputFiles + NAME + "_straight_neutral_sunglasses_"+downsize+".png");
-        greyValuesT[1] = getGreyLevelsFromImages(inputFiles + NAME +"_straight_neutral_open_"+downsize+".png");
-        greyValuesT[2] = getGreyLevelsFromImages(inputFiles + NAME +"_straight_sad_open_"+downsize+".png");
-        greyValuesT[3] = getGreyLevelsFromImages(inputFiles + NAME +"_straight_angry_open_"+downsize+".png");
-        greyValuesT[4] = getGreyLevelsFromImages(inputFiles + NAME +"_up_happy_open_"+downsize+".png");
-        greyValuesT[5] = getGreyLevelsFromImages(inputFiles + NAME +"_up_neutral_open_"+downsize+".png");
-        greyValuesT[6] = getGreyLevelsFromImages(inputFiles + NAME +"_left_happy_open_"+downsize+".png");
-        greyValuesT[7] = getGreyLevelsFromImages(inputFiles + NAME +"_left_neutral_open_"+downsize+".png");
-        greyValuesT[8] = getGreyLevelsFromImages(inputFiles + NAME +"_left_sad_open_"+downsize+".png");
-        greyValuesT[9] = getGreyLevelsFromImages(inputFiles + NAME +"_right_happy_open_"+downsize+".png");
-        greyValuesT[10] = getGreyLevelsFromImages(inputFiles + NAME +"_right_sad_open_"+downsize+".png");
-        greyValuesT[11] = getGreyLevelsFromImages(inputFiles + NAME +"_right_neutral_open_"+downsize+".png");
+        ArrayList<File> files = new ArrayList<File>();
+        File directory = new File(inputDir);
+
+        // get all the files from a directory
+        File[] fList = directory.listFiles();
+        for (File file : fList) {
+            if (file.isFile()) {
+                files.add(file);
+                increasePICTURE_COUNT();
+            }
+        }
+        System.out.println(getPICTURE_COUNT());
+
+        greyValuesT[0] = getGreyLevelsFromImages(inputDir + NAME + "_straight_neutral_sunglasses_" + downsize + ".png");
+        greyValuesT[1] = getGreyLevelsFromImages(inputDir + NAME + "_straight_neutral_open_" + downsize + ".png");
+        greyValuesT[2] = getGreyLevelsFromImages(inputDir + NAME + "_straight_sad_open_" + downsize + ".png");
+        greyValuesT[3] = getGreyLevelsFromImages(inputDir + NAME + "_straight_angry_open_" + downsize + ".png");
+        greyValuesT[4] = getGreyLevelsFromImages(inputDir + NAME + "_up_happy_open_" + downsize + ".png");
+        greyValuesT[5] = getGreyLevelsFromImages(inputDir + NAME + "_up_neutral_open_" + downsize + ".png");
+        greyValuesT[6] = getGreyLevelsFromImages(inputDir + NAME + "_left_happy_open_" + downsize + ".png");
+        greyValuesT[7] = getGreyLevelsFromImages(inputDir + NAME + "_left_neutral_open_" + downsize + ".png");
+        greyValuesT[8] = getGreyLevelsFromImages(inputDir + NAME + "_left_sad_open_" + downsize + ".png");
+        greyValuesT[9] = getGreyLevelsFromImages(inputDir + NAME + "_right_happy_open_" + downsize + ".png");
+        greyValuesT[10] = getGreyLevelsFromImages(inputDir + NAME + "_right_sad_open_" + downsize + ".png");
+        greyValuesT[11] = getGreyLevelsFromImages(inputDir + NAME + "_right_neutral_open_" + downsize + ".png");
 
 
         greyValues = new double[PIXEL_COUNT][PICTURE_COUNT];
 
-        for(int i = 0; i< 12; i++){
-            for (int k = 0; k< PIXEL_COUNT; k++){
+        for (int i = 0; i < 12; i++) {
+            for (int k = 0; k < PIXEL_COUNT; k++) {
                 greyValues[k][i] = greyValuesT[i][k];
             }
         }
@@ -65,7 +80,7 @@ public class PixelToAPDMData {
         return greyValues;
     }
 
-    public int[] getGreyLevelsFromImages(String inputFile) throws IOException{
+    public int[] getGreyLevelsFromImages(String inputFile) throws IOException {
         //get picture and get rgb
 
         try {
@@ -77,25 +92,25 @@ public class PixelToAPDMData {
 
         PIC_HEIGHT = img.getHeight();
         PIC_WIDTH = img.getWidth();
-        PIXEL_COUNT = (int)(Math.ceil(PIC_HEIGHT) * Math.ceil(PIC_WIDTH));
+        PIXEL_COUNT = (int) (Math.ceil(PIC_HEIGHT) * Math.ceil(PIC_WIDTH));
         int[] grayLevels = new int[PIXEL_COUNT];
         System.out.println(PIXEL_COUNT);
-        Arrays.fill(grayLevels,0);
+        Arrays.fill(grayLevels, 0);
 
         //convert to grayscale
         int[] imageRGB = img.getRGB(0, 0, PIC_WIDTH, PIC_HEIGHT, null, 0, PIC_WIDTH);
         int[] tempImageCheck = new int[PIXEL_COUNT];
-        for(int i = 0; i < PIC_HEIGHT * PIC_WIDTH; i++){
+        for (int i = 0; i < PIC_HEIGHT * PIC_WIDTH; i++) {
             int p = imageRGB[i];
             tempImageCheck[i] = p;
 
-    //        int a 	= (imageRGB[i] >> 16) & 0xff;
-            int r 	= (imageRGB[i] >> 16) & 0xff;
-            int g 	= (imageRGB[i] >> 8) & 0xff;
-            int b 	= (imageRGB[i]) & 0xff;
+            //        int a 	= (imageRGB[i] >> 16) & 0xff;
+            int r = (imageRGB[i] >> 16) & 0xff;
+            int g = (imageRGB[i] >> 8) & 0xff;
+            int b = (imageRGB[i]) & 0xff;
             //calculate average(grayVal)
             //grayLevels[i] = (r + g + b) / 3;
-            grayLevels[i] =  p;
+            grayLevels[i] = p;
             r = (r + g + b) / 3;
             g = r;
             b = r;
@@ -120,15 +135,15 @@ public class PixelToAPDMData {
     }
 
 
-    private void generatePixelAPDM(String outputFileName){
+    private void generatePixelAPDM(String outputFileName) {
         //gen edges
         GenerateMNSingleGraph g = new GenerateMNSingleGraph(PIC_HEIGHT, PIC_WIDTH);
         //nothing in count
-        double[] count          = new double[PIXEL_COUNT];
+        double[] count = new double[PIXEL_COUNT];
         Arrays.fill(count, 0.0);
 
         APDMInputFormat.generateAPDMFilePixel("Test", "PixelData", g.edges, this.greyValues,
-                count,  null,  outputFileName);
+                count, null, outputFileName);
     }
 
 
@@ -139,35 +154,36 @@ public class PixelToAPDMData {
         System.out.println(cc.computeCCSubGraph(apdm.data.trueSubGraphNodes));
     }
 
-    public static void main(String args[]) throws IOException{
+    public static void main(String args[]) throws IOException {
 
-        if (NAME.contains("test")){
+        if (NAME.contains("test")) {
             new PixelToAPDMData().generateSingleCase(
                     "data/PixelData/RealData/Images/ImageData/pngFiles/test/",
-                    "data/PixelData/RealData/APDM/APDM-"+NAME);
-        }
-        else if(NAME.equals("")){
-            POSTURE = "Straight";
+                    "data/PixelData/RealData/APDM/APDM-" + NAME);
+        } else if (NAME.equals("")) {
+            Z0NAME = "an2i";
             new PixelToAPDMData().generateSingleCase(
-                    "data/PixelData/RealData/Images/ImageData/pngFiles/"+POSTURE+"_"+downsize,
-                    "data/PixelData/RealData/APDM/APDM-"+POSTURE+"-"+downsize+"-");
-        }
-        else{
+                    "data/PixelData/RealData/Images/ImageData/pngFiles/",
+                    "data/PixelData/RealData/APDM/APDM-" + Z0NAME + "-" + downsize + "-");
+        } else {
             new PixelToAPDMData().generateSingleCase(
-                    "data/PixelData/RealData/Images/ImageData/pngFiles/faces/"+NAME+"/",
-                    "data/PixelData/RealData/APDM/APDM-"+NAME+"-"+downsize+"-");
+                    "data/PixelData/RealData/Images/ImageData/pngFiles/faces/" + NAME + "/",
+                    "data/PixelData/RealData/APDM/APDM-" + NAME + "-" + downsize + "-");
         }
     }
 
-    public BufferedImage getBufferedImage(){
+    public BufferedImage getBufferedImage() {
         return img;
     }
 
-    public int getWidth(){
+    public int getWidth() {
         return PIC_WIDTH;
     }
 
-    public int getHeight(){
+    public int getHeight() {
         return PIC_HEIGHT;
     }
+
+    public int getPICTURE_COUNT(){return PICTURE_COUNT;}
+    public void increasePICTURE_COUNT(){ PICTURE_COUNT = PICTURE_COUNT+1;}
 }
